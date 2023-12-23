256489d62c19e0d7981ca5c959d1379b8bd312ae
clopyta <claudiaz@tlu.ee>
2023-12-23 12:53:15 -0500

    updated

--- script.js
+++ script.js
@@ -2,6 +2,55 @@
 //lin2.tlu.ee redirects me to  https://www.tlu.ee
 // Function to fetch random book title
 async function fetchRandomBook() {
+    const url = 'https://hapi-books.p.rapidapi.com/search/the+walking+dead';
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '5209bdecd9msh126f5e9c3490516p131c73jsn7fc32ea205f3',
+            'X-RapidAPI-Host': 'hapi-books.p.rapidapi.com'
+        }
+    };
+
+    try {
+        const response = await fetch(url, options);
+        const data = await response.json(); // Parsing response as JSON
+        const books = data.results.map(book => book.title).filter(title => title); // Extracting and filtering titles
+        const randomBook = books[Math.floor(Math.random() * books.length)]; // Selecting a random title
+        return randomBook || 'No Book Title Available';
+    } catch (error) {
+        console.error('Error fetching book data:', error);
+        return 'No Book Title Available';
+    }
+}
+
+// Function to fetch random radio station
+async function fetchRandomRadioStation() {
+    const url = 'https://radio-world-75-000-worldwide-fm-radio-stations.p.rapidapi.com/search_radio.php?keyword=hello&limit=10&page=1&order=ASC';
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '5209bdecd9msh126f5e9c3490516p131c73jsn7fc32ea205f3',
+            'X-RapidAPI-Host': 'radio-world-75-000-worldwide-fm-radio-stations.p.rapidapi.com'
+        }
+    };
+
+    try {
+        const response = await fetch(url, options);
+        const data = await response.json(); // Parsing response as JSON
+        const stations = data.result.map(station => station.name).filter(name => name); // Extracting and filtering station names
+        const randomStation = stations[Math.floor(Math.random() * stations.length)]; // Selecting a random station
+        return randomStation || 'No Radio Station Available';
+    } catch (error) {
+        console.error('Error fetching radio station data:', error);
+        return 'No Radio Station Available';
+    }
+}
+
+
+
+
+/*// Function to fetch random book title. the url is a searching it is not random
+async function fetchRandomBook() {
     const url = 'http://openlibrary.org/search.json?q=the+walking+dead';
 
     try {
@@ -30,7 +79,9 @@ async function fetchRandomRadioStation() {
         console.error('Error fetching radio station data:', error);
         return 'Unknown Radio Station';
     }
-}
+}*/
+
+
 
 
 
